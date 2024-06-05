d050537bf46ef0bf403feed3ff4d652c115f8618
aiman <aimanrifq@gmail.com>
2024-06-06 01:11:21 +0800

    codemagic commit

--- android/app/src/androidTest/java/MainActivityTest.java
+++ android/app/src/androidTest/java/MainActivityTest.java
@@ -3,7 +3,7 @@ import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-//import com.example.lab05.MainActivity;
+import com.example.lab05.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
